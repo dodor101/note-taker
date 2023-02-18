ca26fb89d780edfb5c04e64eda5f4bc031dad350
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-02-17 22:50:04 -0600

    Add full functionality for the app

--- server.js
+++ server.js
@@ -1,38 +1,98 @@
-// AS A small business owner
-// I WANT to be able to write and save notes
-// SO THAT I can organize my thoughts and keep track of tasks I need to complete
-
-
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-
-
-// import express
+// Load tools needed
 const express = require('express');
 const path = require('path');
-const PORT = 3001
+const fs = require('fs');
+const dbnotes = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const PORT = process.env.PORT || 3001;
 const app = express();
-// body-parser
-app.use(express.json());
-app.use(express.urlencoded( { extended: true } ));
 
-// serve static file
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// The route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
+// The request for notes
+app.get('/api/notes', (req, res) => {
+  // Send json to the client
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(JSON.parse(data));
 
+    }
+  })
 
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+// POST request for posting notes
+app.post('/api/notes', (req, res) => {
+  console.log(`${req.method} request received to add a note`)
+
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4()
+    }
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote)
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (err) => {
+          err ? console.log(err) : console.log('successfully added note')
+        })
+      }
+    })
+
+    const response = {
+      status: 'success',
+      body: newNote
+    }
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('unable to post note')
+  }
+});
+
+// DELETE request for notes
+app.delete('/api/notes/:id', (req, res) => {
+  console.log(`${req.method} request received to delete a note`)
+  const id = req.params.id;
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    const parsedNotes = JSON.parse(data);
+    const deleteNotes = parsedNotes.filter((note) => note.id !== id);
+
+    fs.writeFile('./db/db.json', JSON.stringify(deleteNotes, null, 4), (err) => {
+      err ? console.log(err) : console.log('successfully deleted note')
+    })
+    res.json(`Note deleted`);
+  })
+});
+
+// Wildcard route to redirect users to landing page
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+// Listen to connection 
 app.listen(PORT, () => {
-  console.info(`listening on port http://localhost:${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
